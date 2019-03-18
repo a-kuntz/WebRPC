5bcc362f4595dcb6ab326b4aec4a69bb07241311
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2019-03-18 22:19:09 +0100

    update build instructions

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@
 pushd .
 mkdir build
 cd build
-cmake .. -DBOOST_ROOT=~/dev/boost_1_69_0 -DCMAKE_BUILD_TYPE=debug
-make
+cmake .. \
+	-DBOOST_ROOT=~/dev/boost_1_69_0 \
+	-DGTEST_ROOT=~/dev/googletest-release-1.8.1/build \
+	-DCMAKE_BUILD_TYPE=debug \
+	-DCMAKE_INSTALL_PREFIX=.
+make all test
 popd
