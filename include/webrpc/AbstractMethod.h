e6cb4d2fced067404fdf0ab2787d0cc409bade4c
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2019-03-22 23:02:41 +0100

    split AbstractMethod impl from spec

--- include/webrpc/AbstractMethod.h
+++ include/webrpc/AbstractMethod.h
@@ -5,12 +5,13 @@
 
 class AbstractMethod : public IMethod
 {
-	public:
 	AbstractMethod() = delete;
-	AbstractMethod(const std::string& name) : _name(name) {}
-	virtual ~AbstractMethod() {};
 
-	std::string get_name() const override {return _name;}
+	public:
+	AbstractMethod(const std::string& name);
+	virtual ~AbstractMethod() = default;
+
+	std::string get_name() const override;
 
 	private:
 	const std::string _name;
