346d34b10a86ae4fe5e192443900f602bbdef055
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2019-03-27 23:12:11 +0100

    add note on how to compile boost

--- script/setup_boost.sh
+++ script/setup_boost.sh
@@ -3,6 +3,7 @@
 set -e
 set -x
 
+# ARCHIVE=~/Downloads/boost_1_68_0.tar.gz
 ARCHIVE=~/Downloads/boost_1_69_0.tar.gz
 DST_DIR=~/deps/
 LIB_ROOT=$(basename ${ARCHIVE} .tar.gz)
@@ -12,5 +13,9 @@ mkdir -p ${DST_DIR}
 cd ${DST_DIR}
 
 tar -xzf ${ARCHIVE}
+# cd ${LIB_ROOT}
+#./bootstrap.sh --show-libraries
+# ./bootstrap.sh --prefix=. --with-libraries=chrono,date_time,system
+# ./b2 install
 
 popd
