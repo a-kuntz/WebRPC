091aea2624b1e95fc3d0ca5250b3e757b0db6ad2
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2019-04-30 23:55:24 +0200

    simplify build.sh

--- script/build.sh
+++ script/build.sh
@@ -6,14 +6,12 @@ set -x
 
 SCRIPT_DIR=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 DEPS_DIR=${SCRIPT_DIR}/../deps
+BUILD_DIR=build
 
-pushd .
-mkdir -p build
-cd build
-cmake .. \
+cmake -B${BUILD_DIR} -H. \
 	-DBOOST_ROOT=${DEPS_DIR}/boost_1_69_0 \
 	-DGTEST_ROOT=${DEPS_DIR}/googletest-release-1.8.1/build \
 	-DCMAKE_BUILD_TYPE=debug \
 	-DCMAKE_INSTALL_PREFIX=.
-CTEST_OUTPUT_ON_FAILURE=1 make all test
-popd
+
+CTEST_OUTPUT_ON_FAILURE=1 make -C ${BUILD_DIR} all test
