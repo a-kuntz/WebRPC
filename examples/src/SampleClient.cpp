4297afe1b331238e24902ae2a287488788bed892
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2019-06-09 23:26:55 +0200

    cleanup commandline arguments

--- examples/src/SampleClient.cpp
+++ examples/src/SampleClient.cpp
@@ -11,44 +11,21 @@
 
 namespace po = boost::program_options;
 
-class IocThreadGuard
-{
-	public:
-	IocThreadGuard(boost::asio::io_context& ioc)
-		: _work(boost::asio::make_work_guard(ioc))
-		, _thread([&]() { ioc.run(); })
-	{}
-
-	~IocThreadGuard()
-	{
-		_thread.join();
-	}
-
-	void reset()
-	{
-		_work.reset();
-	}
-
-	private:
-	boost::asio::executor_work_guard<boost::asio::io_context::executor_type> _work;
-	std::thread																 _thread;
-};
-
 int main(int argc, char** argv)
 {
-	std::string uri;
-
-	po::options_description desc("Options");
+	std::vector<std::string> uris;
+	auto desc = po::options_description{"Options"};
 	desc.add_options()
-		("uri,u", po::value<std::string>(&uri)->value_name("URI"), "WebRPC uri")
-		("verbose,v", "verbose")
+		("async,a", "make asynchronous calls")
+		("uri,u", po::value(&uris), "WebRPC uri")
+		("verbose,v", "be verbose")
 		("help,h", "print help message and exit");
 
-	po::positional_options_description pos_desc;
+	auto pos_desc = po::positional_options_description{};
 	pos_desc.add("uri", -1);
 
 	// parse and compare for required options
-	po::variables_map vm;
+	auto vm = po::variables_map{};
 	po::store(po::command_line_parser(argc, argv).options(desc).positional(pos_desc).run(), vm);
 	po::notify(vm);
 
@@ -64,63 +41,42 @@ int main(int argc, char** argv)
 		return EXIT_SUCCESS;
 	}
 
-	bool verbose = vm.count("verbose") > 0;
+	const auto verbose = vm.count("verbose") > 0;
 
 	try
 	{
 		// create and start io context
-		boost::asio::io_context ioc;
-		Client					c{ioc, verbose};
+		auto ioc = boost::asio::io_context{};
+		auto c = Client{ioc, verbose};
 
-		// async_call(.)
-		//		if (false)
-		{
-			c.async_call(uri, [](const std::string& res) { std::cout << "--" << res << "--\n"; });
-			// c.async_call(uri, [](const std::string& res) { std::cout << "--" << res << "--\n"; });
-			ioc.run();
-		}
+		if (vm.count("async"))
+		{ // make asynchronous call(s)
+			auto guard = boost::asio::make_work_guard(ioc);
 
-		if (false)
-		{
-			c.async_call(uri, [](const std::string& res) { std::cout << "--" << res << "--\n"; });
-			// c.async_call(uri, [](const std::string& res) { std::cout << "--" << res << "--\n"; });
 			std::thread t([&]() { ioc.run(); });
+
+			auto id = 0;
+			for (const auto& uri: uris)
+			{
+				std::cout <<id << "< " << uri << std::endl;
+				c.async_call(uri, [&, id=id](const std::string& res) {
+					std::cout << id << "> " << res << "\n";
+					guard.reset();
+				});
+				++id;
+			}
 			t.join();
 		}
+		else
+		{ // make synchronous call(s)
 
-		if (false)
-		{
-			auto		guard = boost::asio::make_work_guard(ioc);
-			std::thread t([&]() { ioc.run(); });
-			c.async_call(uri, [&](const std::string& res) {
-				std::cout << "--" << res << "--\n";
-				guard.reset();
-			});
-			// c.async_call(uri, [&](const std::string& res) {
-			// 	std::cout << "--" << res << "--\n";
-			// 	guard.reset();
-			// });
-			t.join();
-		}
-
-		if (false)
-		{
-			auto guard = IocThreadGuard{ioc};
-			c.async_call(uri, [&](const std::string& res) {
-				std::cout << "--" << res << "--\n";
-				guard.reset();
-			});
-			// c.async_call(uri, [&](const std::string& res) {
-			// 	std::cout << "--" << res << "--\n";
-			// 	guard.reset();
-			// });
-		}
-
-		// call(.)
-		if (false)
-		{
-			std::cout << "==" << c.call(uri) << "==\n";
-			// std::cout << "==" << c.call(uri) << "==\n";
+			auto id = 0;
+			for (const auto& uri: uris)
+			{
+				std::cout << id << "< "<< uri << std::endl;
+				std::cout << id << "> "<< c.call(uri) << std::endl;
+				++id;
+			}
 		}
 
 		return EXIT_SUCCESS;
