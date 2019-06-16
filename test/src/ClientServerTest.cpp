3910f3eedd19a237e830d74b89dc59c5112a0515
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2019-06-16 23:21:16 +0200

    add async client server tests

--- test/src/ClientServerTest.cpp
+++ test/src/ClientServerTest.cpp
@@ -19,32 +19,41 @@ struct Echo : public AbstractMethod
 };
 }
 
-TEST(ClientServer, Echo)
+TEST(ClientServer, Async)
 {
 	auto ioc = boost::asio::io_context{};
 
 	const unsigned short port = 8080;
 	const boost::asio::ip::address host = boost::asio::ip::make_address("127.0.0.1");
 
-	auto server = Server{ioc, {host, port}, 1};
+	auto server = Server{ioc, {host, port}, 3};
 	server.register_method(std::make_unique<detail::Echo>());
+	server.set_verbose(true);
 	server.start();
 
-	auto received_reply = false;
+	const auto reply_expected = 4;
+	auto reply_cnt = 0;
 	auto client = Client{ioc, false};
 	client.async_call("http://127.0.0.1:8080/system.list_methods", [&](const std::string& res) {
-		received_reply = true;
-		// std::cout << res << std::endl;
 		ASSERT_EQ("[\"Echo\",\"system.list_methods\"]", res);
-		ioc.stop();
+		if (reply_expected == ++reply_cnt) {ioc.stop();}
 	});
 
-	client.async_call("http://127.0.0.1:8080/Echo/Echo", [&](const std::string& res) {
-		received_reply = true;
-		ASSERT_EQ("\"Echo\"", res);
-		ioc.stop();
+	client.async_call("http://localhost:8080/Echo/[{foo:bar},[a,b,c,1,2,3,true,false],<11,12,ab>]", [&](const std::string& res) {
+		ASSERT_EQ("[{foo:\"bar\"},[\"a\",\"b\",\"c\",1,2,3,true,false],<0x11,0x12,0xAB>]", res);
+		if (reply_expected == ++reply_cnt) {ioc.stop();}
+	});
+
+	client.async_call("http://127.0.0.1:8080/Echo/{syntax-error}", [&](const std::string& res) {
+		ASSERT_EQ("null_t", res);
+		if (reply_expected == ++reply_cnt) {ioc.stop();}
+	});
+
+	client.async_call("http://127.0.0.1:8080/Echo/%7Bkey:val%7D", [&](const std::string& res) {
+		ASSERT_EQ("{key:\"val\"}", res);
+		if (reply_expected == ++reply_cnt) {ioc.stop();}
 	});
 
 	ioc.run_for(boost::asio::chrono::seconds(3));
-	ASSERT_TRUE(received_reply);
+	ASSERT_EQ(reply_expected, reply_cnt);
 }
