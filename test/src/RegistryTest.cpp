9b5d597b4c1b71a758447c8a8b446450f58b1785
Andreas Kuntz <50048909+a-kuntz@users.noreply.github.com>
2019-06-25 23:31:00 +0200

    add RegistryTest

---
+++ test/src/RegistryTest.cpp
@@ -0,0 +1,46 @@
+#include <webrpc/AbstractMethod.h>
+#include <webrpc/Registry.h>
+
+#include <gtest/gtest.h>
+
+
+struct Method : public AbstractMethod
+{
+	Method()
+		: AbstractMethod("Method")
+	{}
+
+	boost::optional<value_t> execute(const boost::optional<value_t> arg) override
+	{
+		return arg;
+	}
+};
+
+TEST(Registry, ExistingKey)
+{
+    Registry registry;
+    registry.emplace(std::make_pair("Echo", std::make_unique<Method>()));
+    registry.emplace(std::make_pair("DateTime", std::make_unique<Method>()));
+    registry.emplace(std::make_pair("GetSetValue", std::make_unique<Method>()));
+    registry.emplace(std::make_pair("Revert", std::make_unique<Method>()));
+    registry.emplace(std::make_pair("Shuffle", std::make_unique<Method>()));
+    registry.emplace(std::make_pair("Sum", std::make_unique<Method>()));
+    registry.emplace(std::make_pair("system.list_methods", std::make_unique<Method>()));
+
+    {
+        const auto res = registry.find("Echo");
+        ASSERT_FALSE(res == registry.cend());
+        ASSERT_TRUE(res != registry.cend());
+    }
+
+    {
+        const auto res = registry.find("system.list_methods");
+        ASSERT_TRUE(res != registry.cend());
+    }
+
+    {
+        const auto res = registry.find("favicon.ico");
+        ASSERT_TRUE(res == registry.cend());
+    }
+
+}
